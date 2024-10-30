afb97960909d82d6ef4bfd682672e4d54422a699
dylanlaborde <dylan4047@gmail.com>
2024-10-30 08:17:01 +0100

    Create all the route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Signin from './Pages/Signin/Signin'
+import Home from './Pages/Home/Home'
+import Profile from './Pages/Profile/Profile'
 
+function App() {
   return (
     <>
-      <div>App</div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/sign-in' element={<Signin />}></Route>
+          <Route path='/profile' element={<Profile />}></Route> 
+        </Routes>
+      </Router>    
     </>
   )
 }
