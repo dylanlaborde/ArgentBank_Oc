f64193c9c7124c5eb0a8915ad58cc8b72c44c526
dylanlaborde <dylan4047@gmail.com>
2024-10-31 09:41:57 +0100

    Create the Store and first reducer (initial)

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './Assets/css/main.css'
 import App from './App.jsx'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import rootReducer from './Reducers'
+
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools:true
+})
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
