071acf6911d720b7bb817f51b83561004ab6c8f5
Dylan <dylan4047@gmail.com>
2024-11-18 23:01:36 +0100

    update

--- src/Reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import { authReducer } from "./authReducer";
-import { userReducer } from "./userReducer";
-
-export default combineReducers({
-    auth: authReducer,
-    user:userReducer
-})
